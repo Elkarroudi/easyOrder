5d7d7a14dc487d86478c39a21623593b68427677
Asmaa Hariti <haritiasmaa74@gmail.com>
2025-01-22 23:54:11 +0100

    implementing pagination and improving total price appearing in the home page

--- frontEnd/easyOrder/src/app/components/food-menu/food-menu.component.ts
+++ frontEnd/easyOrder/src/app/components/food-menu/food-menu.component.ts
@@ -1,20 +1,24 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HttpClient} from '@angular/common/http';
-import {Dish} from '../../models/dish.model';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Dish } from '../../models/dish.model';
 
 @Component({
   selector: 'app-food-menu',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './food-menu.component.html'
+  templateUrl: './food-menu.component.html',
 })
 export class FoodMenuComponent implements OnInit {
   private http = inject(HttpClient);
 
+  currentPage = 0;
   dishes: Dish[] = [];
+  paginatedDishes: Dish[] = [];
   cartItems: { [key: number]: number } = {};
   total = 0;
+  itemsPerPage = 8;
+  totalPages = 0;
 
   ngOnInit(): void {
     this.fetchDishes();
@@ -22,12 +26,39 @@ export class FoodMenuComponent implements OnInit {
 
   fetchDishes(): void {
     this.http.get<Dish[]>('http://localhost:8085/api/dishes').subscribe(data => {
-      console.log("Full data response: ", data);
-
-      console.log("rating is ", data.map(dish => dish.rating));
       this.dishes = data;
+      this.totalPages = Math.ceil(this.dishes.length / this.itemsPerPage);
+      this.updatePaginatedDishes();
     });
   }
+
+  updatePaginatedDishes(): void {
+    const start = this.currentPage * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.paginatedDishes = this.dishes.slice(start, end);
+  }
+
+  goToPage(page: number): void {
+    if (page >= 0 && page < this.totalPages) {
+      this.currentPage = page;
+      this.updatePaginatedDishes();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.updatePaginatedDishes();
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages - 1) {
+      this.currentPage++;
+      this.updatePaginatedDishes();
+    }
+  }
+
   updateQuantity(id: number, delta: number): void {
     const currentQty = this.cartItems[id] || 0;
     const newQty = currentQty + delta;
@@ -49,3 +80,4 @@ export class FoodMenuComponent implements OnInit {
     );
   }
 }
+

31b31301b7f9e4e88ca89bb5cee8176ca6b1211e
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2025-02-03 14:07:43 +0100

    fix: add inject import in food-menu component

--- FrontEnd/src/app/components/food-menu/food-menu.component.ts
+++ FrontEnd/src/app/components/food-menu/food-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Store } from '@ngrx/store';
